b97d1d4861a38abe6249479901e4936cb32660ef
Lorenzo.Gao <lorenzo.gao@ug.uchile.cl>
2024-09-29 23:34:59 +0800

    feat(protagonist.tscn): added hit_box and hurt_box to protagonist and respective function: take_damage(damage)

--- jugo/protagonist.gd
+++ jugo/protagonist.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends CharacterBody2D
 
 
@@ -9,6 +10,8 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -23,3 +26,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func take_damage(damage: int):
+	queue_free()
