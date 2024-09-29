7e44415d598d9daecee945005e376f1a7d17fe3c
Lorenzo.Gao <lorenzo.gao@ug.uchile.cl>
2024-09-29 23:29:13 +0800

    feat: added two objects and their respective functions: hit_box.gd, hurt_box.gd

---
+++ jugo/hurt_box.gd
@@ -0,0 +1,22 @@
+class_name Hurtbox
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	pass # Replace with function body.
+
+
+func _on_area_entered(area: Area2D) -> void:
+	var hitbox = area as Hitbox
+	if hitbox:
+		if owner.has_method("take_damage"):
+			owner.take_damage(hitbox.damage)
+			hitbox.damage_dealt.emit()
+			
+	
+
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
