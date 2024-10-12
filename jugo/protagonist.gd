449ff1bfff225f15905d0ae8e59dddaa335fef6f
SebastianAguileraM <sebastian.aguilera.m@ug.uchile.cl>
2024-10-12 19:21:11 -0300

    FEAT: added the run, fall and jump for the player. And added the crown.

--- jugo/protagonist.gd
+++ jugo/protagonist.gd
@@ -2,30 +2,66 @@ class_name Player
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+
+@export var speed = 400
+@export var jump_speed = 700
+@export var gravity = 1500
+@export var acceleration = 2000
+@export var attacking = false
+@onready var pivot: Node2D = $Pivot
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/playback")
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
+	var move_input = Input.get_axis("move_left", "move_right")
+	if attacking:
+		move_input = 0
+	velocity.x = move_toward(velocity.x, speed *  move_input, acceleration * delta)
 	
-	
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_speed
 	move_and_slide()
+	
+	if move_input != 0:
+		pivot.scale.x = sign(move_input)
+	
+	if is_on_floor():
+		if abs(velocity.x) > 10 or move_input:
+			playback.travel("run")
+		else:
+			playback.travel("idle")
+	else:
+		if velocity.y < 0:
+			playback.travel("jump")
+		else:
+			playback.travel("fall")
+
+
+#func _physics_process(delta: float) -> void:
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+	#
+	#
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = -jump_speed
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * speed
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, speed)
+#
+	#move_and_slide()
 
 
 func take_damage(damage: int):
