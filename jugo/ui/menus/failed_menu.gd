a0d41ec0bdf34d62e32ec0e145264a4b370a0000
Lorenzo.Gao <lorenzo.gao@ug.uchile.cl>
2024-11-05 04:08:00 +0800

    fix: solved another conflicts and override the path in failedMenu and victoryMenu to sample.tscn.

--- jugo/ui/menus/failed_menu.gd
+++ jugo/ui/menus/failed_menu.gd
@@ -12,7 +12,8 @@ func _ready() -> void:
 func _on_restart_pressed() -> void:
 	get_tree().paused = false
 	#get_tree().change_scene_to_file("res://main.tscn")
-	get_tree().reload_current_scene()
+	#get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://levels/Sample.tscn")
 	
 	
 func _on_mainMenu_pressed() -> void:
