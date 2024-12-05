694c8c15c1617fa06dcfd50c89ba8f178453c594
SebastianAguileraM <sebastian.aguilera.m@ug.uchile.cl>
2024-12-05 20:50:48 -0300

    Cambié de rama con el godot abeirto, aaaaaaah, todo mal, ayudame dios.

--- jugo/scenes/characters/protagonist/protagonist.gd
+++ jugo/scenes/characters/protagonist/protagonist.gd
@@ -2,6 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 
+
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
 
@@ -19,19 +20,56 @@ extends CharacterBody2D
 @onready var kill_enemy_sound: AudioStreamPlayer = $Kill_enemy_sound
 
 
+@onready var tilemap : TileMapLayer = get_parent().get_node("TileMapLayerInteractive")
+
+@onready var esta_enganchado = false
+
+@onready var cerca_de_cuerda = false
+
+func _ready():
+	# Deferimos la conexión para asegurar que los nodos existen
+	call_deferred("_setup_connections")
+	
+func _setup_connections():
+	$"Area2D".connect("body_entered", Callable(self, "_on_area_2d_body_entered"))
+	$"Area2D".connect("body_exited", Callable(self, "_on_area_2d_body_exited"))
+
+func _on_area_2d_body_entered(body):
+	#print("hola")
+	if body == tilemap:
+		#print("hola2")
+		cerca_de_cuerda = true
+		
+func _on_area_2d_body_exited(body):
+	cerca_de_cuerda = false
+	esta_enganchado = false
+	
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
+	if not is_on_floor() and esta_enganchado == false:
 		velocity.y += gravity * delta
 	var move_input = Input.get_axis("move_left", "move_right")
 	if attacking:
 		move_input = 0
 	velocity.x = move_toward(velocity.x, speed *  move_input, acceleration * delta)
 	
+	if cerca_de_cuerda == true and esta_enganchado == false:
+		#print("¡vamo a engancharno!")
+		if Input.is_action_just_pressed("rope"):
+			velocity.y = 0
+			esta_enganchado = true
+			print(esta_enganchado)
+			# Aquí va la lógica para engancharse (por ejemplo, el jugador no puede moverse, se mueve con la cuerda, etc.)
+			#print("¡Jugador enganchado a la cuerda!")
+			#velocity.y = 0
+			
+	if cerca_de_cuerda == false:
+		esta_enganchado = false
+	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_speed
 		jump_sound.play()
-	move_and_slide()
+	#move_and_slide()
 	
 	if move_input != 0:
 		pivot.scale.x = sign(move_input)
@@ -52,33 +90,23 @@ func _physics_process(delta: float) -> void:
 			playback.travel("jump")
 		else:
 			playback.travel("fall")
-
+			
+			
+			
+	if tilemap == null:
+		print("No hay cuerdas.")
+		return
+		
+	if esta_enganchado == true and Input.is_action_just_pressed("jump"):
+		esta_enganchado = false
+		cerca_de_cuerda = false
+		
+	move_and_slide()
 
 
 func pickup(item: String):
 	print("Item!!")
 	InventoryManager.add_item(item)
-
-#func _physics_process(delta: float) -> void:
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-	#
-	#
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = -jump_speed
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * speed
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, speed)
-#
-	#move_and_slide()
 
 
 func take_damage(damage: int):
@@ -88,6 +116,7 @@ func take_damage(damage: int):
 	Manager._go_to_failed_menu()
 	
 	
-func on_area_entered(lever: Area2D) -> void:
-	lever.turn_level(1)
-	#Input.is_action_pressed("lever")
+
+
+#func _on_area_2d_body_entered(body: Node2D) -> void:
+	#pass # Replace with function body.
