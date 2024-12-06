e0f3f72b441335a07f9d9fc2f59c0dc1850e0355
Lorenzo.Gao <lorenzo.gao@ug.uchile.cl>
2024-12-06 10:17:31 +0800

    style: added another artists's name.

--- jugo/ui/menus/credits.gd
+++ jugo/ui/menus/credits.gd
@@ -1,14 +1,18 @@
-extends Control
 
-@onready var button: Button = $Button
+extends Control
 @onready var rich_text_label: RichTextLabel = $RichTextLabel
+@onready var button: Button = $Button
 
 # Called when the node enters the scene tree for the first time.
+ 
+
 func _ready() -> void:
 	await get_tree().create_timer(3).timeout
 	button.pressed.connect(Manager._go_to_menu)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rich_text_label.get_v_scroll_bar().value += delta * 10
+	
+	
+	
